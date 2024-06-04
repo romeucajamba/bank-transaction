c745c85356de823452c3ac3ea17cc9288427081b
romeucajamba <romeucajamba@gmail.com>
2024-06-04 22:27:57 +0100

    feat: testing get route with transactions list

--- src/test/transactions.test.ts
+++ src/test/transactions.test.ts
@@ -1,18 +1,22 @@
-import { describe, test, beforeAll, afterAll } from 'vitest';
+import { describe, test, beforeAll, afterAll, expect } from 'vitest';
 import request from 'supertest';
 import { server } from '../app';
-
-beforeAll( async () => {
-    await server.ready()
-})
-
-afterAll( async () => {
-    await server.close()
-})
+import { title } from 'process';
 
 
-describe('create transaction', () => {
-    test('shold return 201, that mean the transaction created', async () => {
+
+describe('Transactions routes', () => {
+
+    beforeAll( async () => {
+        await server.ready()
+    })
+    
+    afterAll( async () => {
+        await server.close()
+    })
+    
+
+    test.only('shold return 201, that mean the transaction created', async () => {
         
       await request(server.server).post('/transactions').send({
         title: 'New transaction created on test',
@@ -20,5 +24,25 @@ describe('create transaction', () => {
         type: 'credit'
       }).expect(201)
 
+    })
+
+    test('Get all transactions', async () => {
+       
+        const createNewTransaction = await request(server.server).post('/transactions').send({
+            title: 'New transaction created on test',
+            amount: 500,
+            type: 'credit'
+          })
+          
+          const cookies = createNewTransaction.get('Set-Cookie')
+
+          const transactionResponse = await request(server.server).get('/transactions').expect(200)
+          
+          expect(transactionResponse.body).toEqual([
+            expect.objectContaining({
+                title: 'New transaction created on test',
+                amount: 500
+            }),
+          ])
     })
 })
