3fde5bbef1e7ec6485d7f1e88431efa3531986e2
romeucajamba <romeucajamba@gmail.com>
2024-06-05 17:18:22 +0100

    change the port

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import { server } from './app';
+const port = process.env.PORT || 4000;
 
 server.listen({
-    port: 3333
+    port: port
 }).then(() => {
     console.log('server running in port 3333')
 })
